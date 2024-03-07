d2cc8c6eeef723bfb89f1987a430742bb797819d
Felipe Fernandes Alves <112494130+PatoDeSapatos@users.noreply.github.com>
2024-03-07 11:43:35 -0300

    Binary Search algorithm

---
+++ BinarySearch.js
@@ -0,0 +1,58 @@
+function randomInt(min, max) {
+    return Math.floor((Math.random() * max) + min);
+}
+
+const array = [];
+const length = randomInt(20, 10000000);
+let iterations = 0;
+
+for (let i = 0; i < length; i++) {
+    array[i] = randomInt(0, 10000000);
+}
+
+const targetPos = randomInt(0, length);
+const target = array[ targetPos ];
+array.sort((a,b) => a-b);
+
+console.log("Finished alimentation");
+console.log(`The target is: ${target} \nAnd the array length is: ${length} \n------------------------------------------------`);
+
+let offset = 0;
+let limit = array.length;
+let timer = Date.now();
+while (true) {
+    const element = Math.round( (offset+limit)/2 );
+
+    if ( array[ element ] > target ) {
+        limit = element;
+    } else if ( array[element] < target ) {
+        offset = element;
+    } else {
+        console.log(`Target Found: ${array[ element ]}`);
+        break;
+    }
+    iterations++;
+}
+
+console.log(`Target position in array: ${targetPos} \nTime: ${Date.now() - timer}ms \nNumber of iterations: ${iterations} \n------------------------------------------------`);
+iterations = 0;
+
+timer = Date.now();
+for (let i = 0; i < array.length; i++) {
+    if (array[i] == target) {
+        console.log(`Target Found: ${array[ i ]}`);
+        break;
+    }
+    iterations++;
+}
+
+console.log(`Target position in array: ${targetPos} \nTime: ${Date.now() - timer}ms \nNumber of iterations: ${iterations}`);
+
+// offset = 0;
+// limit = 200;
+
+// offset = 100;
+// limit = 200;
+
+// offset = 100;
+// limit = 150 ((offset+limit)/2);
